60f96a261b29ef97b30fd94b039aef49db1870c5
Brad Dunbar <dunbarb2@gmail.com>
2016-05-08 10:14:35 -0400

    First commit.

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Vendor
+const ozymandias = require('ozymandias')
+
+// The App!
+const app = module.exports = ozymandias()
+
+// Routes
+app.get('/', (req, res) => res.render('index'))
+
+// 404
+app.get('*', (req, res) => res.status(404).render('404'))
+
+// 500
+app.use((e, req, res, next) => {
+  console.log(e.stack)
+  res.status(500).render('500')
+})
---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const port = process.env.PORT
+require('./app').listen(port)
+
+console.log(`Listening on port ${port}`)
